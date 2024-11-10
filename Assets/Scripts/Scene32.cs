89ba7e3d5b6834d83f51b10fc67368d13a301c87
wwonnn <dnjsgml0508@naver.com>
2024-11-10 17:01:54 +0900

    build version

--- Assets/Scripts/Scene32.cs
+++ Assets/Scripts/Scene32.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Data;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class Scene32 : MonoBehaviour
 {
     public GameObject choicePanel;
     public GameObject change_conv1;
     public GameObject change_conv2;
 
+    public string next_scene;
     public int index = 0;
     bool choice = false;
 
@@ -57,6 +60,7 @@ public class Scene32 : MonoBehaviour
                     if (index > change_conv1.transform.childCount)
                     {
                         index = 2;
+                        StartCoroutine(ChangeScene(next_scene));
                     }
                 }
                 else
@@ -73,6 +77,7 @@ public class Scene32 : MonoBehaviour
                     if (index >= change_conv2.transform.childCount)
                     {
                         index = 2;
+                        StartCoroutine(ChangeScene(next_scene));
                     }
                 }
                 }
@@ -80,28 +85,36 @@ public class Scene32 : MonoBehaviour
         
     }
 
-        public void Choice1()
-        {
-            choice = true;
-            Debug.Log(0);
-            // 1번 선택
-            choicePanel.SetActive(false);
-            change_conv1.SetActive(true);
-            choice_num = 1;
-            index = 0;
-        }
+    public void Choice1()
+    {
+        choice = true;
+        Debug.Log(0);
+        // 1번 선택
+        choicePanel.SetActive(false);
+        change_conv1.SetActive(true);
+        choice_num = 1;
+        index = 0;
+    }
 
-        public void Choice2()
-        {
-            choice = true;
-            Debug.Log(1);
-            // 2번 선택
-            choicePanel.SetActive(false);
-            change_conv2.SetActive(true);
-            choice_num = 2;
-            index = 0;
+    public void Choice2()
+    {
+        choice = true;
+        Debug.Log(1);
+        // 2번 선택
+        choicePanel.SetActive(false);
+        change_conv2.SetActive(true);
+        choice_num = 2;
+        index = 0;
 
-        }
+    }
 
-    } 
+    IEnumerator ChangeScene(string sceneName)
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        // 씬 전환
+        SceneManager.LoadScene(sceneName);
+    }
+
+} 
 
